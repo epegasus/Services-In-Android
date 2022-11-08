c8b72b2f6ad9f41dea2f50e779597d155f2c19e1
Muzammil-Hussain <mh66966910@gmail.com>
2022-11-08 15:16:26 +0500

    Services

--- app/src/main/java/com/funsol/technologies/services/MyService.kt
+++ app/src/main/java/com/funsol/technologies/services/MyService.kt
@@ -1,15 +1,71 @@
 package com.funsol.technologies.services
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
-import android.os.Binder
-import android.os.Handler
-import android.os.IBinder
-import android.os.Looper
+import android.os.*
 import android.util.Log
+import androidx.annotation.RequiresApi
 
 
+@Suppress("DEPRECATION")
 class MyService : Service() {
+
+    companion object {
+        private const val TAG = "MyService"
+        const val EXTRA_NOTIFICATION_REQUEST_CODE = "EXTRA_NOTIFICATION_REQUEST_CODE"
+        const val EXTRA_NOTIFICATION = "EXTRA_NOTIFICATION"
+
+        fun showNotification(context: Context, requestCode: Int, notification: Notification): Boolean {
+            val intent = Intent(context, MyService::class.java)
+            intent.putExtra(EXTRA_NOTIFICATION_REQUEST_CODE, requestCode)
+            intent.putExtra(EXTRA_NOTIFICATION, notification)
+            return startService(context, intent)
+        }
+
+        fun stop(context: Context) {
+            val intent = Intent(context, MyService::class.java)
+            context.stopService(intent)
+        }
+
+        private fun startService(context: Context, intent: Intent): Boolean {
+            //
+            // Similar to ContextCompat.startForegroundService(context, intent)
+            //
+            val componentName: ComponentName? = if (Build.VERSION.SDK_INT >= 26) {
+                context.startForegroundService(intent)
+            } else {
+                context.startService(intent)
+            }
+            return componentName != null
+        }
+
+        @RequiresApi(api = 26)
+        fun createNotificationChannel(
+            context: Context,
+            id: String, name: String, importance: Int,
+            description: String
+        ) {
+            val channel = NotificationChannel(id, name, importance)
+            channel.description = description
+            createNotificationChannel(context, channel)
+        }
+
+        @Suppress("MemberVisibilityCanBePrivate")
+        @RequiresApi(api = 26)
+        fun createNotificationChannel(
+            context: Context,
+            channel: NotificationChannel
+        ) {
+            val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
     private val mBinder: IBinder = MyBinder()
     private var mHandler: Handler? = null
     var progress = 0
@@ -22,9 +78,35 @@ class MyService : Service() {
         private set
 
     override fun onCreate() {
+        Log.d(TAG, "+onCreate()")
         super.onCreate()
+        Log.d(TAG, "-onCreate()")
         initValues()
     }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        try {
+            Log.d(TAG, "+onStartCommand(...)")
+            if (intent != null) {
+                val extras = intent.extras
+                if (extras != null) {
+                    if (extras.containsKey(EXTRA_NOTIFICATION)) {
+                        val notification = extras.getParcelable<Parcelable>(EXTRA_NOTIFICATION)
+                        if (notification is Notification) {
+                            if (extras.containsKey(EXTRA_NOTIFICATION_REQUEST_CODE)) {
+                                val requestCode = extras.getInt(EXTRA_NOTIFICATION_REQUEST_CODE)
+                                startForeground(requestCode, notification)
+                            }
+                        }
+                    }
+                }
+            }
+            return START_NOT_STICKY
+        } finally {
+            Log.d(TAG, "-onStartCommand(...)")
+        }
+    }
+
 
     private fun initValues() {
         mHandler = Handler(Looper.myLooper()!!)
@@ -34,7 +116,23 @@ class MyService : Service() {
     }
 
 
-    override fun onBind(intent: Intent): IBinder = mBinder
+    override fun onBind(intent: Intent): IBinder {
+        try {
+            Log.d(TAG, "+onBind(...)")
+            return mBinder
+        } finally {
+            Log.d(TAG, "-onBind(...)")
+        }
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        try {
+            Log.d(TAG, "+onUnbind(...)")
+            return true
+        } finally {
+            Log.d(TAG, "-onUnbind(...)")
+        }
+    }
 
 
     inner class MyBinder : Binder() {
@@ -78,11 +176,10 @@ class MyService : Service() {
     }
 
     override fun onDestroy() {
+        Log.d(TAG, "+onDestroy()")
+        //PbLog.s(TAG, PbStringUtils.separateCamelCaseWords("onDestroy"));
         super.onDestroy()
-        Log.d(TAG, "onDestroy: called.")
-    }
-
-    companion object {
-        private const val TAG = "MyService"
+        stopForeground(true)
+        Log.d(TAG, "-onDestroy()")
     }
 }
